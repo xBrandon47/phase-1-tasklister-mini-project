85b573088e841ece0abd6fb0dba328cfaa27a0b2
Brandon Moreno <brandonm99@me.com>
2022-09-26 17:12:24 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    })
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#create-task-form').apprendChild(p)
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
